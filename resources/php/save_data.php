3fb4093fc65eb6a1f4e855b56e03a04b7c6b494d
Davi Alves Oliveira <davi-ao@users.noreply.github.com>
2019-02-07 12:10:43 -0300

    0.1.0 - Added timestamp to trial

--- resources/php/save_data.php
+++ resources/php/save_data.php
@@ -5,7 +5,7 @@ $data = $_POST['data'];
 try {
     $conn = new PDO("mysql:host=localhost;dbname=db_ingles",
         'root', 'acessoroot', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $sql = "INSERT INTO rst VALUES(0, '{$type}', '{$data}')";
+    $sql = "INSERT INTO rst VALUES(0, '{$type}', '{$data}', NOW()";
     $conn->query($sql);
 
     echo $sql;
